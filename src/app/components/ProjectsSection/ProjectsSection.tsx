91146308258ab5ffca0c1f52afdf1368b1bd56a6
jowjow22 <jonathan224santos@gmail.com>
2025-04-22 15:26:28 -0300

    fix: escape problem

--- src/app/components/ProjectsSection/ProjectsSection.tsx
+++ src/app/components/ProjectsSection/ProjectsSection.tsx
@@ -11,15 +11,12 @@ export const ProjectsSection = () => {
   return (
     <Card variant="image_background">
       <Card.Header>
-        <div className="flex flex-col items-start w-full">
-          <Typography.Paragraph>April 20, 2023</Typography.Paragraph>
-          <Typography.H3 className="font-bold text-white!">
-            Project Title
-          </Typography.H3>
-        </div>
         <Button onlyIcon icon={<IconMaximize size={20} />} />
       </Card.Header>
       <Card.Content>
+        <Typography.H3 className="font-bold text-white!">
+          Project Title
+        </Typography.H3>
         <p className="text-ellipsis whitespace-nowrap overflow-hidden">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
