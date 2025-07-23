ba47f22bdd517cdf7ae7e34cc02ee2639013f466
jowjow22 <jonathan224santos@gmail.com>
2025-07-23 17:33:36 -0300

    feat: add revalidation to articles fetching

--- src/app/server/actions/articles.ts
+++ src/app/server/actions/articles.ts
@@ -2,11 +2,15 @@
 
 import { env } from '@/lib/env'
 import { Article } from '@/lib/types/articles'
+
 export async function fetchArticles(): Promise<Article[]> {
   const res = await fetch('https://dev.to/api/articles/me/published', {
     headers: {
       'api-key': env.DEV_API_KEY || '',
     },
+    next: {
+      revalidate: 3600 * 24, // 24 hours
+    }
   })
 
   return res.json()
